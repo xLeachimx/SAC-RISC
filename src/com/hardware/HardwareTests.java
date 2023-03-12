a1e150a771b850e2ee8d45614865a88f79182654
xLeachimx <michael.huelsman@gmail.com>
2023-03-11 22:53:33 -0500

    Hard disk redesign

---
+++ src/com/hardware/HardwareTests.java
@@ -0,0 +1,16 @@
+/* File: HardwareTests.java
+ * Author: Dr. Michael Andrew Huelsman
+ * Created On: 11 Mar 2023
+ * Licence: GNU GPLv3
+ * Purpose:
+ * Notes:
+ */
+
+
+package com.hardware;
+
+public class HardwareTests {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/com/hardware/Interupts.java
@@ -0,0 +1,21 @@
+/* File: Interupts.java
+ * Author: Dr. Michael Andrew Huelsman
+ * Created On: 11 Mar 2023
+ * Licence: GNU GPLv3
+ * Purpose:
+ * Notes:
+ */
+
+
+package com.hardware;
+
+public enum Interupts {
+    //Exit Codes
+    NO_ERROR,
+    HARDDISK_ACCESS_ERROR,
+    HARDDISK_CLOSE_ERROR,
+    HARDDISK_SEEK_ERROR,
+    HARDDISK_READ_ERROR,
+    HARDDISK_WRITE_ERROR,
+    HARDDISK_RETRIEVAL_ERROR,
+}
